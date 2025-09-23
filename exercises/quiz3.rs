6fbdb41730262aaa7e0f6b3a83f1f80468e5c0f9
aaravlu <demolemon@outlook.at>
2025-09-23 19:37:25 +0800

    fix: Complete multiple Rustlings exercises on hashmaps and quizzes

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -2,7 +2,7 @@
 //
 // This quiz tests:
 // - Generics
-// - Traits 
+// - Traits
 //
 // An imaginary magical school has a new report card generation system written
 // in Rust! Currently the system only supports creating report cards where the
@@ -16,18 +16,36 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+pub trait Grade {
+    fn display(&self) -> String;
+}
+
+impl Grade for f32 {
+    fn display(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Grade for &str {
+    fn display(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            self.grade.display()
+        )
     }
 }
 
@@ -52,7 +70,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
