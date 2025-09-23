8972b6ccc7cd6e9f798777966b4c035d409b9fc2
aaravlu <demolemon@outlook.at>
2025-09-23 19:59:50 +0800

    fix: Complete exercises for Rust Options

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
@@ -12,8 +10,14 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
-    // TODO: Complete the function body - remember to return an Option!
-    ???
+    // Return 5 pieces if before 10PM (22:00), 0 pieces if 10PM or later, None if time is invalid
+    if time_of_day > 23 {
+        None
+    } else if time_of_day < 22 {
+        Some(5)
+    } else {
+        Some(0)
+    }
 }
 
 #[cfg(test)]
@@ -31,9 +35,8 @@ mod tests {
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get at the value contained in the
-        // Option?
+        // Get the value contained in the Option using unwrap()
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap(), 5);
     }
 }
